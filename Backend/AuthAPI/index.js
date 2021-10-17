c983621a8aafe49ce76fde2d03256a2fd6e0e0e6
AlvinC888 <chengalvin333@gmail.com>
2021-10-17 21:22:51 +1100

    FInished up get all users and create user endpoint

--- Backend/AuthAPI/index.js
+++ Backend/AuthAPI/index.js
@@ -1,18 +1,56 @@
 //Importing packages
 import express from "express";
-
+import { URI } from "./vars.js";
+import user from "./schema.js";
+import mongoose from "mongoose";
 //Express sestup
 const app = express();
 
 //Variables
 const PORT = 8080;
 
+//Db connection
+mongoose.connect(URI);
+
+//Json middleware
+app.use(express.json);
+
 //Get endpoint
 app.get("/", (req, res) => {
   res.status(401).send("REQUEST ERROR: PLEASE ENTER API KEY.");
 });
 
-//Notfound error handling
+//Create user endpoint
+app.post("/api/users/post/create", (req, res) => {
+  //create user
+  try {
+    const newUser = new user({
+      id: req.body.id,
+      username: req.body.username,
+      password: req.body.password,
+    });
+    newUser.save().then(() => {
+      res.status(201).send("Done, no errors and problems.");
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//Find users endpoint
+app.get("/api/users/get/all", (req, res) => {
+  //Find all users
+  try {
+    user
+      .find()
+      .exec()
+      .then((data) => res.status(200).send(data));
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//Not found error handling
 const notFound = (req, res) => {
   res.status(404).send("REQUEST ERROR: The page you requested was not found.");
 };
