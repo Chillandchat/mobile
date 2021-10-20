eab4da1db85d3bbcc2797e8772369aa4f24fed4d
AlvinC888 <chengalvin333@gmail.com>
2021-10-20 21:11:00 +1100

    Create Credit component

---
+++ Frontend/app/src/components/Credit.tsx
@@ -0,0 +1,14 @@
+//Importing packeages
+import React from "react";
+import "./style/Credit.css";
+
+//Credit component
+export const Credit: React.FC = () => {
+  //Render component
+  return (
+    <div>
+      {/*Credit*/}
+      <p id="credit">Created by Brother and sister software.</p>
+    </div>
+  );
+};
