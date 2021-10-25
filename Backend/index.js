39b6451f633d8e829674e31a9e4b846ff7f4c1cf
AlvinC888 <chengalvin333@gmail.com>
2021-10-25 19:31:18 +1100

    Revert "CORS Update and minor endpoint changes"
    This reverts commit 589e36a6d59d92bfc1bb9a56f63ae3fdd2ce2a99.

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,36 @@
 //Importing packages
 const app = require("express")();
 const URI = require("./vars.js");
-const user = require("./schema.js");
+const express = require("express");
+const user = require("./authSchema.js");
+const message = require("./messageSchema.js");
 const mongoose = require("mongoose");
-const io = require("socket.io");
-const server = require("http").server(app);
+const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const cors = require("cors");
 
 //Variables
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || "8080";
 
 //Web socket
 io.on("connection", (socket) => {
   console.log("User connected.");
-  socket.on("message", (message) => {
-    io.emit(message);
-  });
+});
+io.on("message", (payload) => {
+  io.emit("message", payload);
+  try {
+    const newMessage = new message({
+      id: payload.id,
+      user: payload.user,
+      content: payload.content,
+    });
+    newMessage.save().then(() => {
+      res
+        .status(201)
+        .send("Message saved successfully, no errors or problems.");
+    });
+  } catch (err) {
+    res.status(500).send(`SERVER ERROR: ${err}`);
+  }
 });
 
 //Db connection
@@ -25,12 +39,21 @@ mongoose.connect(URI);
 //Json middleware
 app.use(express.json());
 
-//Cross origin access control
-app.use(cors());
-
 //Get endpoint
 app.get("/", (req, res) => {
   res.status(401).send("REQUEST ERROR: PLEASE ENTER API KEY.");
+});
+
+//Get all message endpoint
+app.get("/api/messages/get", (req, res) => {
+  try {
+    message
+      .find()
+      .exec()
+      .then((data) => res.status(200).send(data));
+  } catch (err) {
+    res.status(500).send(`SERVER ERROR: ${err}`);
+  }
 });
 
 //Create user endpoint
@@ -41,12 +64,14 @@ app.post("/api/users/post/create", (req, res) => {
       id: req.body.id,
       username: req.body.username,
       password: req.body.password,
+      blocked: req.body.blocked,
+      onDeleteList: req.body.onDeleteList,
     });
     newUser.save().then(() => {
-      res.status(201).send("Done, no errors and problems.");
+      res.status(201).send("Saved successfully, no errors and problems.");
     });
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).send(`SERVER ERROR: ${err}`);
   }
 });
 
@@ -59,13 +84,55 @@ app.get("/api/users/get/all", (req, res) => {
       .exec()
       .then((data) => res.status(200).send(data));
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).send(`SERVER ERROR: ${err}`);
   }
+});
+
+//Login endpoint
+app.get("/api/users/get/:user/:password", (req, res) => {
+  try {
+    user
+      .findOne({ username: req.params.user, password: req.params.password })
+      .exec()
+      .then((data) => {
+        res.status(200).send("User login successfully");
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(`SERVER ERROR: ${err}`);
+  }
+});
+
+//Find user endpoint
+app.get("/api/users/get/:user/", (req, res) => {
+  try {
+    user
+      .findOne({ username: req.params.user })
+      .exec()
+      .then((data) => {
+        res.status(200).send("User found");
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(`SERVER ERROR: ${err}`);
+  }
+});
+
+//Block user endpoint
+app.put("/api/users/block/", (req, res) => {
+  user.findOne({ username: req.body.user }, (err, data) => {
+    if (err) res.status(500).send(err);
+    else data.blocked = req.body.blockStatus;
+  });
 });
 
 //Not found error handling
 const notFound = (req, res) => {
-  res.status(404).send("REQUEST ERROR: The page you requested was not found.");
+  res
+    .status(404)
+    .send(
+      "REQUEST ERROR: The page you requested was not found, please type a valid URL."
+    );
 };
 app.use(notFound);
 
