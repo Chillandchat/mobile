81eb07e73e09d3fbf2028efc6f15cc04967e2225
AlvinC888 <chengalvin333@gmail.com>
2021-10-29 10:11:55 +1100

    Creating signup page (not complete WIP)

--- Frontend/app/src/components/SignUpPage.tsx
+++ Frontend/app/src/components/SignUpPage.tsx
@@ -1,9 +1,32 @@
 import React from "react";
-
+import { Icon } from "./Icon";
+import { LoginForm } from "./LoginForm";
+import { Link } from "react-router-dom";
+import "./style/Signup.css";
 export const SignUpPage: React.FC = () => {
   return (
-    <div>
-      <p>asdf</p>
+    <div id="signupParent">
+      <Icon />
+      <div id="form">
+        <LoginForm
+          password={false}
+          formPlaceHolder="Your username"
+          formLabel="Username"
+        />
+        <LoginForm
+          password={true}
+          formPlaceHolder="Your password"
+          formLabel="Password"
+        />
+        <LoginForm
+          password={true}
+          formPlaceHolder="Confirm password"
+          formLabel="Password"
+        />
+      </div>
+      <Link to="/" id="loginLink">
+        Have an account? Login Here!
+      </Link>
     </div>
   );
 };
