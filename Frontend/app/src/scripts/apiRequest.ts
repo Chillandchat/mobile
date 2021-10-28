d4ed74588d90f74bec65d7f46e6e608ca22abb9f
AlvinC888 <chengalvin333@gmail.com>
2021-10-29 10:10:59 +1100

    Completed api request

--- Frontend/app/src/scripts/apiRequest.ts
+++ Frontend/app/src/scripts/apiRequest.ts
@@ -1,14 +1,27 @@
 //Importing packages
 import axios from "axios";
 
+//Endpoint interface config
+export interface apiEndpointType {
+  getAllMessageEndpoint: String;
+  loginEndpoint: String;
+  signupEndpoint: String;
+  getAllUsersEndpoint: String;
+  findUserEndpoint: String;
+  blockUsersEndpoint: String;
+}
+
 //Api endpoints
-export const apiEndpoints = {
-  loginEndpoint: "/api/users/get/all",
-  signupEndpoint: "/api/users/create.post",
+export const apiEndpoints: apiEndpointType = {
+  getAllMessageEndpoint: "/api/messages/get",
+  getAllUsersEndpoint: "/api/users/get/all",
+  signupEndpoint: "/api/users/create/post",
+  loginEndpoint: "/api/users/get/",
+  findUserEndpoint: "/api/users/get/",
+  blockUsersEndpoint: "/api/users/block/",
 };
 
 //Axios for api requests
 export const api = axios.create({
-  mode: "no-cors",
   baseURL: "http://localhost:8080",
 });
