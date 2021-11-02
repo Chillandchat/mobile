aee533ce3191af1446d5efaf62f29ae36177100d
AlvinC888 <chengalvin333@gmail.com>
2021-11-02 15:50:31 +1100

    API bug fixes

--- Frontend/app/src/scripts/login.ts
+++ Frontend/app/src/scripts/login.ts
@@ -1,15 +1,28 @@
+//Importing packages
 import { apiEndpoints as endpoints, api } from "./apiRequest";
 
-const login = (username: String, password: String) => {
+//login function  
+const login = (username: string, password: string): any => {
+  //Check if username and password is empty
+  if (username == undefined || password == undefined) {
+    //Throw error
+    console.error("username and password must be provided");
+    return;
+  }
+  //Contact api
   try {
     api
       .get(`${endpoints.loginEndpoint}${username}/${password}`)
       .then((response) => {
+        //TODO - Modify redux state
         console.log(response);
       });
-  } catch (err) {
+  }
+  //Catch error s
+  catch (err) {
     console.error(err);
+    return;
   }
 };
-
+//Export login function
 export default login;
