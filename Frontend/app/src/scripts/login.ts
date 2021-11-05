ee0d9a7c401815d7ec111fb6dac67c3576099893
AlvinC888 <chengalvin333@gmail.com>
2021-11-06 10:59:57 +1100

    Login script complete

--- Frontend/app/src/scripts/login.ts
+++ Frontend/app/src/scripts/login.ts
@@ -1,28 +1,44 @@
 //Importing packages
-import { apiEndpoints as endpoints, api } from "./apiRequest";
+import { apiEndpoints, api } from "./apiRequest";
 
-//login function  
-const login = (username: string, password: string): any => {
+//login function
+export const login = async (
+  username: string,
+  password: string
+): Promise<boolean> => {
+  //Ok status variable
+  let okStatus: boolean = false;
+
   //Check if username and password is empty
-  if (username == undefined || password == undefined) {
+  if (username === undefined || password === undefined) {
     //Throw error
-    console.error("username and password must be provided");
-    return;
+    console.error(
+      "Uncaught Error: username and password must be provided\n  do not leave field blank\n   at <input> LoginPage.tsx\nIf you have found a bug, please report bug at: https://github.com/AlvinC888/Chill-chat/issues"
+    );
+    //Change ok status
+    okStatus = false;
+  } else {
+    okStatus = true;
   }
-  //Contact api
-  try {
-    api
-      .get(`${endpoints.loginEndpoint}${username}/${password}`)
-      .then((response) => {
-        //TODO - Modify redux state
-        console.log(response);
-      });
+  //Check if ok status is true
+  if (okStatus) {
+    //Contact api
+    try {
+      await api
+        .get(`${apiEndpoints.loginEndpoint}${username}/${password}`)
+        .then((response) => {
+          //Check API response status
+          if (response.status === 404 || response.status === 400) {
+            okStatus = false;
+          }
+          if (response.status === 200) okStatus = true;
+        });
+    } catch (err) {
+      //Throw error
+      console.error(err);
+      okStatus = false;
+    }
   }
-  //Catch error s
-  catch (err) {
-    console.error(err);
-    return;
-  }
+  //Return status
+  return okStatus;
 };
-//Export login function
-export default login;
