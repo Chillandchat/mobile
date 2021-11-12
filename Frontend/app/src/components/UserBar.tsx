0d77d0531397ae0967ad2295e747739c620e2286
AlvinC888 <chengalvin333@gmail.com>
2021-11-12 17:26:59 +1100

    Finish user bar

---
+++ Frontend/app/src/components/UserBar.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { getAllUsers } from "../scripts/getAllUsers";
+import {UserBarIcon } from "./UserBarIcon";
+import "./style/UserBar.css";
+
+export const UserBar: React.FC = () => {
+  let [userList, setUserList] = useState<any>([]);
+  useEffect(() => {
+    getAllUsers().then((returnData) => {
+      setUserList(returnData.data);
+    });
+  }, []);
+  return (
+    <div id="userList">
+      {userList.map((user) => {
+        return (
+          <div id="user">
+            <UserBarIcon/>
+            <strong className="userDisplayName" key={user.id}>
+              {user.username}
+            </strong>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
