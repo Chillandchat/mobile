8104c7b67ccd78e508b1176759e9b11386970bbe
AlvinC888 <chengalvin333@gmail.com>
2021-11-12 17:26:29 +1100

    Update ChatRoom.tsx

--- Frontend/app/src/components/ChatRoom.tsx
+++ Frontend/app/src/components/ChatRoom.tsx
@@ -1,33 +1,43 @@
 import React from "react";
 import "./style/ChatRoom.css";
 import { InputField } from "./InputField";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../redux/reducers/index";
 import { Redirect } from "react-router-dom";
 import { SendButton } from "./SendButton";
-import { getAllMessage } from "../scripts/getAllMessages";
+import { UserBar } from "./UserBar";
+import { Logout } from "./Logout";
+import { logout, clearUsername } from "../redux/action";
+
 export const ChatRoom: React.FC = () => {
+  let users: any;
   const username = useSelector((state: RootState) => {
     return state.username;
   });
   const authenticated = useSelector((state: RootState) => {
     return state.login;
   });
-
+  const dispatch = useDispatch();
   if (authenticated) {
     return (
       <div id="chatRoom">
+        <Logout
+          onClick={() => {
+            dispatch(logout());
+            dispatch(clearUsername());
+          }}
+        />
         <p>Hi {username}! and welcome to chill and chat🚀</p>
         <div id="sendBar">
           <InputField placeholder="Type a message..." />
           <SendButton
             onclick={() => {
-              getAllMessage().then((data) => {
-                console.log(data);
-              });
+              console.log(users);
+              console.log("Hello!");
             }}
           />
         </div>
+        <UserBar />
       </div>
     );
   }
