ccb9e5774113beba7598d32b0c74235cb71b887e
AlvinC888 <chengalvin333@gmail.com>
2021-11-13 13:01:51 +1100

    Complete user bar

--- Frontend/app/src/components/UserBar.tsx
+++ Frontend/app/src/components/UserBar.tsx
@@ -1,21 +1,45 @@
+//Import packages
 import React, { useEffect, useState } from "react";
 import { getAllUsers } from "../scripts/getUsers";
-import {UserBarIcon } from "./UserBarIcon";
+import { UserBarIcon } from "./UserBarIcon";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 import "./style/UserBar.css";
 
-export const UserBar: React.FC = () => {
+//Props interface
+interface Props {
+  viewOnClick: () => void;
+}
+
+//UserBar component
+export const UserBar: React.FC<Props> = (props) => {
+  //State
   let [userList, setUserList] = useState<any>([]);
+
+  //Call get user function
   useEffect(() => {
+    //Call function
     getAllUsers().then((returnData) => {
       setUserList(returnData.data);
     });
   }, []);
+
+  //Render component
   return (
     <div id="userList">
+      {/*Hide/unhide button*/}
+      <VisibilityOffIcon id="hideButton" onClick={props.viewOnClick} />
+      {/*Tittle*/}
+      <h2 id="tittle">Users:</h2>
+      {/*Map users*/}
       {userList.map((user) => {
+        {
+          /*Render user*/
+        }
         return (
           <div id="user">
-            <UserBarIcon/>
+            {/*Icon*/}
+            <UserBarIcon />
+            {/*Username*/}
             <strong className="userDisplayName" key={user.id}>
               {user.username}
             </strong>
