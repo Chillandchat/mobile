737940ecadc8e2f5b2f476327e0a962ebd2aafb5
AlvinC888 <chengalvin333@gmail.com>
2021-11-13 12:51:42 +1100

    Chat room component half complete

--- Frontend/app/src/components/ChatRoom.tsx
+++ Frontend/app/src/components/ChatRoom.tsx
@@ -1,3 +1,4 @@
+//Importing packages
 import React, { useState } from "react";
 import "./style/ChatRoom.css";
 import { InputField } from "./InputField";
@@ -6,60 +7,65 @@ import { RootState } from "../redux/reducers/index";
 import { Redirect } from "react-router-dom";
 import { SendButton } from "./SendButton";
 import { UserBar } from "./UserBar";
-import { Logout } from "./Logout";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import { logout, clearUsername } from "../redux/action";
+import { Menu } from "./Menu";
 
+//Chat room component
 export const ChatRoom: React.FC = () => {
-  let [view, setView] = useState<boolean>(false);
+  //State
+  let [view, setView] = useState(false);
+  //Redux state
   const username = useSelector((state: RootState) => {
     return state.username;
   });
   const authenticated = useSelector((state: RootState) => {
     return state.login;
   });
-  const dispatch = useDispatch();
-  if (authenticated) {
+
+  //Render user component
+  if (view) {
     return (
-      <div id="chatRoom">
-        <div id="menu">
-          <Logout
-            onClick={() => {
-              dispatch(logout());
-              dispatch(clearUsername());
-            }}
-          />
-          <VisibilityIcon
-            className="viewButton"
-            onClick={() => {
-              setView(true);
-            }}
-          />
-        </div>
-        <p>Hi {username}! and welcome to chill and chat🚀</p>
-        <div id="sendBar">
-          <InputField placeholder="Type a message..." />
-          <SendButton
-            onclick={() => {
-              console.log("Hello!");
-            }}
-          />
-        </div>
+      <div>
+        {/*UserBar*/}
+        <UserBar
+          viewOnClick={() => {
+            //Change state 
+            setView(false);
+          }}
+        />
       </div>
     );
   }
+  //Redirect user if not authenticated
   if (!authenticated) {
     return (
       <div>
+        {/*Redirect user*/}
         <Redirect to="/" />
       </div>
     );
   }
-  if (view) {
-    return (
-      <div>
-        <p>HI</p>
+  //Render default component
+  return (
+    <div id="chatRoom">
+      {/*Menu*/}
+      <Menu
+        viewOnClick={() => {
+          //Change state
+          setView(true);
+        }}
+      />
+      {/*Send bar*/}
+      <div id="sendBar">
+        {/*Input field*/}
+        <InputField placeholder="Type a message..." />
+        {/*Send button*/}
+        <SendButton
+          onclick={() => {
+            //TODO - Send message
+            console.log("Hello!");
+          }}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 };
