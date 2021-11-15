44b63ae3084182acd7f67ea04219dfdae3fe3063
AlvinC888 <chengalvin333@gmail.com>
2021-11-15 21:34:48 +1100

    Update UserBarIcon.tsx

--- Frontend/app/src/components/UserBarIcon.tsx
+++ Frontend/app/src/components/UserBarIcon.tsx
@@ -1,11 +1,14 @@
+//Import packages
 import React from "react";
 import Icon from "../content/User.png";
 import "./style/UserBarIcon.css";
 
-
+//User bar icon
 export const UserBarIcon: React.FC = () => {
+  //Render component
   return (
     <div id="wrapper">
+      {/*Icon*/}
       <img src={Icon} alt="Error" id="icon" />
     </div>
   );
