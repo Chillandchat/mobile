7562f78b07f171646c6e81aae5b75edcb7eb2ec7
AlvinC888 <chengalvin333@gmail.com>
2021-11-15 21:35:17 +1100

    Update InputField.tsx

--- Frontend/app/src/components/InputField.tsx
+++ Frontend/app/src/components/InputField.tsx
@@ -1,12 +1,18 @@
+//Importing packages
 import React from "react";
 import "./style/InputField.css";
+
+//Props interface
 interface Props {
   placeholder: string;
 }
 
+//Input field component
 export const InputField: React.FC<Props> = (props) => {
+  //Render component
   return (
     <div>
+      {/*Input field*/}
       <input id="inputField" placeholder={props.placeholder} />
     </div>
   );
