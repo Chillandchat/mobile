440979c00ac879fe883d3c55c752bad9c5daba20
AlvinC888 <chengalvin333@gmail.com>
2021-11-15 21:34:42 +1100

    Update chat bubble component

--- Frontend/app/src/components/ChatBubble.tsx
+++ Frontend/app/src/components/ChatBubble.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Message } from "./Message";
+import "./style/ChatBubble.css";
 
-export const ChatBubble:React.FC = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
- 
+export const ChatBubble: React.FC = () => {
+  return (
+    <div id="chatBubble">
+      <Message
+        user="hi"
+        content="hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi"
+      />
+      <Message content="hi" user="hi" />
+      <Message content="hi" user="hi" />
+      <Message content="hi" user="hi" />
+      <Message content="hi" user="hi" />
+      <Message content="hi" user="hi" />
+      <Message content="hi" user="hi" />
+    </div>
+  );
+};
