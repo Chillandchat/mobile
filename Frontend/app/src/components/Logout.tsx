940a2740736c53fa1513c63dc6a270f5cf17f6c2
AlvinC888 <chengalvin333@gmail.com>
2021-11-15 21:35:21 +1100

    Update Logout.tsx

--- Frontend/app/src/components/Logout.tsx
+++ Frontend/app/src/components/Logout.tsx
@@ -1,12 +1,19 @@
+//Importing packages
 import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
 import "./style/Logout.css";
+
+//Props interface
 interface Props {
   onClick: () => void;
 }
+
+//logout component
 export const Logout: React.FC<Props> = (props) => {
+  //Render component
   return (
     <div>
+      {/*Logout icon*/}
       <HomeIcon onClick={props.onClick} id="logout" />
     </div>
   );
