689a86855533290ec9c2420c5b7d7099ad17640f
AlvinC888 <chengalvin333@gmail.com>
2021-11-17 20:10:50 +1100

    Change branch

--- Frontend/app/src/components/ChatBubble.tsx
+++ Frontend/app/src/components/ChatBubble.tsx
@@ -1,20 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Message } from "./Message";
+import { getAllMessage } from "../scripts/getAllMessages";
 import "./style/ChatBubble.css";
 
 export const ChatBubble: React.FC = () => {
+  let [messages, setMessages] = useState([]);
+  useEffect(() => {
+    getAllMessage().then((message) => {
+      setMessages(message.messages);
+    });
+  }, []);
   return (
     <div id="chatBubble">
-      <Message
-        user="hi"
-        content="hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi"
-      />
-      <Message content="hi" user="hi" />
-      <Message content="hi" user="hi" />
-      <Message content="hi" user="hi" />
-      <Message content="hi" user="hi" />
-      <Message content="hi" user="hi" />
-      <Message content="hi" user="hi" />
+      {messages.map((message) => {
+        return (
+          <div>
+            <Message user={message.user} content={message.content} />
+          </div>
+        );
+      })}
     </div>
   );
 };
