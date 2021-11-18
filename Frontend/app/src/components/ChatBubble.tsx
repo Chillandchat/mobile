35abbe2aa266dbf98152e3d4c7c47920c93ec9dc
AlvinC888 <chengalvin333@gmail.com>
2021-11-18 20:58:57 +1100

    Complete chat bubble component(Ready for testing)

--- Frontend/app/src/components/ChatBubble.tsx
+++ Frontend/app/src/components/ChatBubble.tsx
@@ -1,24 +1,80 @@
-import React, { useEffect, useState } from "react";
+//Importing packages
+import React, { useEffect, useState, useRef } from "react";
 import { Message } from "./Message";
+import { Message as MessageType } from "../scripts/types";
+import dotenv from "dotenv";
+import { io } from "socket.io-client";
 import { getAllMessage } from "../scripts/getAllMessages";
 import "./style/ChatBubble.css";
 
+//SocketIO connection
+const socket = io(process.env.REACT_APP_WEBSOCKET_URL);
+
+//Chat bubble component
 export const ChatBubble: React.FC = () => {
-  let [messages, setMessages] = useState([]);
+  //Show message handler reference
+  const showMessageHandler = useRef<any>(null);
+
+  //Message state
+  let [messages, setMessages] = useState<Array<MessageType | any>>([]);
+
+  //Use effect to run code
   useEffect(() => {
+    //Get all messages from api
     getAllMessage().then((message) => {
+      //Set state
       setMessages(message.messages);
     });
+
+    //Env configuration
+    dotenv.config();
+
+    /*eslint-disable-next-line*/
   }, []);
+
+  useEffect(() => {
+    //SocketIO listener
+    socket.on("message", (chatMessage: MessageType) => {
+      //New message variable
+      let newMessage: Array<MessageType> = messages;
+
+      //Assign and set message
+      newMessage = [...messages, chatMessage];
+      setMessages([]);
+      setMessages(newMessage);
+    });
+    /*eslint-disable-next-line*/
+  });
+
+  useEffect(() => {
+    //Scroll handler
+    showMessageHandler.current.scrollIntoView();
+  }, [messages]);
+
+  //Render messages
   return (
     <div id="chatBubble">
       {messages.map((message) => {
-        return (
-          <div>
-            <Message user={message.user} content={message.content} />
-          </div>
-        );
+        //Render no message
+        if (message == []) {
+          return <div></div>;
+        }
+        //Render messages
+        else {
+          return (
+            <div>
+              {/*Message*/}
+              <Message
+                key={message.id}
+                user={message.user}
+                content={message.content}
+              />
+            </div>
+          );
+        }
       })}
+      {/*Scroll handler*/}
+      <div ref={showMessageHandler} id="showMessageHandler" />
     </div>
   );
 };
