f3ec4ea521e73b8ae100bdd759452eebf6f3181a
AlvinC888 <chengalvin333@gmail.com>
2022-01-21 13:58:50 +1100

    maintain backend api

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -6,47 +6,50 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
+import { AuthSchemaType, MessageSchemaType } from "./type";
 
 // Setup dotenv
 dotenv.config();
 
 // Setup express
-const app: any = express();
+const app: express.Express = express();
 
 // Variables
-const port: string | number = process.env.PORT || "8080";
+const port: string = String(process.env.PORT) || "8080";
 
 // Db connection
-mongoose.connect(process.env.API_URI);
+mongoose.connect(String(process.env.API_URI));
 
 // Json middleware
 app.use(express.json());
 
 // CORS middleware
-app.use(
-  cors({
-    origin: /*"https://chill-and-chat-web.web.app"*/ '*',
-  })
-);
+app.use(cors({}));
 
 // Get endpoint
-app.get("/", (req: any, res: any): void => {
+app.get("/", (_req: any, res: any, _next: any): void => {
+  // Send error
   res.status(401).send("REQUEST ERROR: PLEASE ENTER API KEY.");
 });
 
 // Get all message endpoint
-app.get("/api/get_all_message", (req: any, res: any): void => {
-  try {
-    // Search database
-    message
-      .find()
-      .exec()
-      .then((data: any): void => res.status(200).send(data));
-  } catch (err) {
-    // Throw error
-    res.status(500).send(`SERVER ERROR: ${err}`);
+app.get(
+  "/api/get-messages",
+  async (_req: any, res: any, _next: any): Promise<void> => {
+    try {
+      // Search database
+      await message
+        .find()
+        .exec()
+        .then((data: Array<MessageSchemaType>): void =>
+          res.status(200).send(data)
+        );
+    } catch (err: unknown) {
+      // Throw error
+      res.status(500).send(`SERVER ERROR: ${err}`);
+    }
   }
-});
+);
 
 // Update message like count
 //  app.put('/api/update_like_count', (req, res) => {
@@ -58,158 +61,183 @@ app.get("/api/get_all_message", (req: any, res: any): void => {
 //  })
 
 // Create user endpoint
-app.post("/api/sign_up", (req: any, res: any): void => {
-  try {
-    // Create user element
-    const newUser: any = new user({
-      id: req.body.id,
-      username: req.body.username,
-      password: req.body.password,
-      verified: req.body.verified,
-      bot: req.body.bot,
-      blocked: req.body.blocked,
-    });
-    // Save database
-    newUser.save().then((): void => {
-      res.status(201).send("Saved successfully, no errors and problems.");
-    });
-  } catch (err) {
-    // Throw error
-    res.status(500).send(`SERVER ERROR: ${err}`);
+app.post(
+  "/api/signup",
+  async (req: any, res: any, _next: any): Promise<void> => {
+    try {
+      // Create user element
+      const newUser: any = new user({
+        id: req.body.id,
+        username: req.body.username,
+        password: req.body.password,
+        verified: req.body.verified,
+        bot: req.body.bot,
+        blocked: req.body.blocked,
+      });
+      // Save database
+      await newUser.save().then((): void => {
+        // Send info
+        res.status(201).send("Saved successfully, no errors and problems.");
+      });
+    } catch (err: unknown) {
+      // Throw error
+      res.status(500).send(`SERVER ERROR: ${err}`);
+    }
   }
-});
+);
 
 // Find all users endpoint
-app.get("/api/get_all_users", (req: any, res: any): void => {
-  try {
-    // Search database
-    user
-      .find()
-      .exec()
-      .then((data: any): void => res.status(200).send(data));
-  } catch (err) {
-    // Throw error
-    res.status(500).send(`SERVER ERROR: ${err}`);
+app.get(
+  "/api/get-users",
+  async (_req: any, res: any, _next: any): Promise<void> => {
+    try {
+      // Search database
+      await user
+        .find()
+        .exec()
+        .then((data: Array<AuthSchemaType>): void =>
+          res.status(200).send(data)
+        );
+    } catch (err: unknown) {
+      // Throw error
+      res.status(500).send(`SERVER ERROR: ${err}`);
+    }
   }
-});
+);
 
 // Login endpoint
-app.post("/api/login", (req: any, res: any): void => {
-  try {
-    // Search database
-    user
-      .findOne({ username: req.body.user })
-      .exec()
-      .then((data: any): void => {
-        // Check if user exists
-        if (data != null && data != undefined) {
-          // Check password
-          if (
-            data.username === req.body.user &&
-            data.password === req.body.password
-          )
-            /*Handle conditions:*/ res.status(200).send("User login success");
-          else res.status(400).send("Invalid password");
-        } else res.status(404).send("User not found");
-      })
-      .catch((err: any): void => {
-        // Throw error
-        res.status(500).send(`SERVER ERROR: ${err}`);
-      });
-  } catch (err) {
-    // Throw error
-    res.status(500).send(`SERVER ERROR: ${err}`);
+app.post(
+  "/api/login",
+  async (req: any, res: any, _next: any): Promise<void> => {
+    try {
+      // Search database
+      await user
+        .findOne({ username: req.body.user })
+        .exec()
+        .then((data: AuthSchemaType | null | undefined): void => {
+          // Check if user exists
+          if (data != null && data != undefined) {
+            // Check password
+            if (
+              data.username === req.body.user &&
+              data.password === req.body.password
+            )
+              /*Handle conditions:*/ res.status(200).send("User login success");
+            else res.status(400).send("Invalid password");
+          } else res.status(404).send("User not found");
+        })
+        .catch((err: unknown): void => {
+          // Throw error
+          res.status(500).send(`SERVER ERROR: ${err}`);
+        });
+    } catch (err: unknown) {
+      // Throw error
+      res.status(500).send(`SERVER ERROR: ${err}`);
+    }
   }
-});
+);
 
 // Find user endpoint
-app.get("/api/get_user/:user/", (req: any, res: any): void => {
-  try {
-    // Find user
-    user
-      .findOne({ username: req.params.user })
-      .exec()
-      .then((data: any): void => {
-        // Check conditions
-        if (data != null || data != undefined) res.status(200).send(data);
-        /*Throw error:*/ else res.status(404).send("User not found");
-      })
-      .catch((err: any): void => {
-        // Throw error
-        res.status(500).send(`SERVER ERROR: ${err}`);
-      });
-  } catch (err) {
-    // Throw error
-    res.status(500).send(`SERVER ERROR: ${err}`);
+app.get(
+  "/api/get-user-info",
+  async (req: any, res: any, _next: any): Promise<void> => {
+    try {
+      // Find user
+      await user
+        .findOne({ username: req.query.user })
+        .exec()
+        .then((data: AuthSchemaType | null | undefined): void => {
+          // Check conditions
+          if (data != null || data != undefined) res.status(200).send(data);
+          /*Throw error:*/ else res.status(404).send("User not found");
+        })
+        .catch((err: unknown): void => {
+          // Throw error
+          res.status(500).send(`SERVER ERROR: ${err}`);
+        });
+    } catch (err: unknown) {
+      // Throw error
+      res.status(500).send(`SERVER ERROR: ${err}`);
+    }
   }
-});
-app.post("/api/report_user", (req: any, res: any): void => {
-  // Email ok
-  let emailOk: boolean = false;
+);
+app.post(
+  "/api/report-user",
+  async (req: any, res: any, _next: any): Promise<void> => {
+    // Email ok
+    let emailOk: boolean = false;
 
-  // Error message
-  let error: string;
+    // Error message
+    let error: string;
 
-  // Transporter
-  const transporter: any = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.API_EMAIL,
-      pass: process.env.API_EMAIL_PASS,
-    },
-  });
+    // Transporter
+    const transporter: typeof nodemailer.createTransport =
+      nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.API_EMAIL,
+          pass: process.env.API_EMAIL_PASS,
+        },
+      });
 
-  // Mail options
-  const mailOptions: any = {
-    from: process.env.API_EMAIL,
-    to: "chengalvin333@gmail.com",
-    subject: "You have a new report from the Chill&chat server",
-    text: `${req.body.user} has reported ${req.body.reportUser}'s message.\nMessage: '${req.body.reason}'\n`,
-  };
+    // Mail options
+    const mailOptions: any = {
+      from: process.env.API_EMAIL,
+      to: "chengalvin333@gmail.com",
+      subject: "You have a new report from the Chill&chat server",
+      text: `${req.body.user} has reported ${req.body.reportUser}'s message.\nMessage: '${req.body.reason}'\n`,
+    };
 
-  // Send mail
-  transporter.sendMail(mailOptions, (err: any, data: any): void => {
-    // Check errors
-    if (err) {
-      error = err;
-      emailOk = false;
-    } else emailOk = true;
-  });
+    // Send mail
+    await transporter.sendMail(
+      mailOptions,
+      (err: any, _data: any, _next: any): void => {
+        // Check errors
+        if (err) {
+          error = err;
+          emailOk = false;
+        } else emailOk = true;
+      }
+    );
 
-  // Send status
-  if (emailOk) res.status(200).send();
-  else res.status(500).send(`SERVER ERROR: ${error}`);
-});
+    // Send status
+    if (emailOk) res.status(200).send();
+    else res.status(500).send(`SERVER ERROR: ${error}`);
+  }
+);
 
 // Block user endpoint
-app.put("/api/block_user", (req: any, res: any): void => {
-  // Error variable
-  let error: boolean = false;
+app.put(
+  "/api/block_user",
+  async (req: any, res: any, _next: any): Promise<void> => {
+    // Error variable
+    let error: boolean = false;
 
-  // Find and update user
-  user
-    .findOneAndUpdate({ username: req.body.user })
-    .exec()
-    .then((data: any): void => {
-      // Check conditions
-      if (data != null || data != undefined) {
-        try {
-          // Change data
-          data.blocked = req.body.blockedStatus;
-          data.save();
-        } catch (err) {
-          // Throw error
-          error = true;
-        }
-      } /*Throw error:*/ else res.status(404).send("User not found");
-      if (!error) /*Check conditions:*/ res.status(200).send();
-      // Reset status
-      error = false;
-    });
-});
+    // Find and update user
+    await user
+      .findOneAndUpdate({ username: req.body.user })
+      .exec()
+      .then((data: AuthSchemaType | null | undefined): void => {
+        // Check conditions
+        if (data != null || data != undefined) {
+          try {
+            // Change data
+            data.blocked = req.body.blockedStatus;
+            data.save();
+          } catch (err: unknown) {
+            // Throw error
+            error = true;
+          }
+        } /*Throw error:*/ else res.status(404).send("User not found");
+        if (!error) /*Check conditions:*/ res.status(200).send();
+        // Reset status
+        error = false;
+      });
+  }
+);
 
 // Not found error handling
-const notFound = (req: any, res: any): void => {
+const notFound = (_req: any, res: any, _next: any): void => {
   // Throw error
   res
     .status(404)
