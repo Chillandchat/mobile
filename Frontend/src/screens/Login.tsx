fe9876d2193926e28c7d986b8da6e26c4273fbf9
AlvinC888 <chengalvin333@gmail.com>
2022-02-01 13:50:55 +1100

    Update login page

--- Frontend/src/screens/Login.tsx
+++ Frontend/src/screens/Login.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Button from "../components/Button";
+import Form from "../components/LoginForm";
 
-const Login: React.FC = () => {
+/**
+ * This is the login component for the application, this component is responsible for
+ * rendering the login components and loading login data.
+ */
+
+const Login: React.FC<any> = ({ navigation }) => {
+  let [error, setError] = React.useState(false);
+
   const style: any = StyleSheet.create({
     container: {
       justifyContent: "center",
@@ -10,12 +18,42 @@ const Login: React.FC = () => {
       flex: 1,
     },
     text: {
-      fontFamily: "poppins",
+      fontFamily: "poppinsExtraBold",
+      fontSize: 35,
+      marginBottom: 30,
+      marginLeft: 55,
+      alignSelf: "flex-start",
+    },
+    formContainer: {
+      marginBottom: 40,
+    },
+    signup: {
+      margin: 15,
+      fontFamily: "poppinsExtraBold",
+    },
+    error: {
+      color: "red",
+      marginTop: -20,
+      marginBottom: 20,
+      fontFamily: "poppinsLight",
     },
   });
   return (
     <View style={style.container}>
       <Text style={style.text}>Login</Text>
+      <View style={style.formContainer}>
+        <Form title="Username" safeEntry={false} type="username" />
+        <Form title="Username" safeEntry={true} type="password" />
+        <Text
+          style={style.signup}
+          onPress={() => navigation.navigate("sign-up")}
+        >
+          No account? signup!
+        </Text>
+      </View>
+      {error ? (
+        <Text style={style.error}>Invalid username or password.</Text>
+      ) : null}
       <Button
         onPress={(): void => {
           //! DUMMY FUNCTION - REMOVE BEFORE PRODUCTION
---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { FormProps as Props } from "../types/props";
+import { Feather } from "@expo/vector-icons";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  ScaledSize,
+  Text,
+} from "react-native";
+
+/**
+ *  This is the Login form element of the login page.
+ *
+ *  @param {string} title The title of the form.
+ *  @param {boolean} safeEntry Whetherthe form has a safe entry feature.
+ *  @type {loginFormTypeSelector} The type of the form. @see props.d.ts
+ *  @param {any} value The state of the value of the form.
+ *  @param {void} onTextChange The callback of the onTextChange callback event.
+ */
+
+const LoginForm: React.FC<Props> = (props: Props) => {
+  const windowDimensions: ScaledSize = Dimensions.get("window");
+
+  const style: any = StyleSheet.create({
+    container: {
+      margin: 15,
+      width: windowDimensions.width / 1.4,
+      borderBottomWidth: 2,
+      borderTopColor: "transparent",
+      borderLeftColor: "transparent",
+      borderRightColor: "transparent",
+      borderBottomColor: "#E5E5E5",
+    },
+    headerContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginBottom: 15,
+    },
+    text: {
+      fontFamily: "poppinsBold",
+      fontSize: 19,
+    },
+    input: {
+      marginBottom: 12,
+    },
+  });
+
+  return (
+    <View style={style.container}>
+      <View style={style.headerContainer}>
+        <Text style={style.text}>
+          {props.type === "username" ? "Username" : "Password"}
+        </Text>
+        <Feather
+          name={props.type === "username" ? "user" : "key"}
+          size={24}
+          color="black"
+        />
+      </View>
+      <TextInput
+        placeholder=""
+        value={props.value}
+        onChangeText={props.onTextChange}
+        secureTextEntry={props.safeEntry}
+        style={style.input}
+      />
+    </View>
+  );
+};
+
+export default LoginForm;
