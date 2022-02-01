839938abfc158d76a5a160e2ac1ca4d78d3e51c4
AlvinC888 <chengalvin333@gmail.com>
2022-02-01 13:50:37 +1100

    Update props.d.ts

--- frontend/src/types/props.d.ts
+++ frontend/src/types/props.d.ts
@@ -10,3 +10,23 @@ export interface ButtonProps {
   onPress: () => void;
   textColor: string;
 }
+
+/**
+ * This is the props for the form component.
+ *
+ * @see Form.tsx
+ */
+
+export interface FormProps {
+  title: string;
+  safeEntry: boolean;
+  type: loginFormTypeSelector;
+  value?: any;
+  onTextChange?: (text: string) => void;
+}
+
+/**
+ * This is the type ristrictions for the form component type prop.
+ */
+
+type loginFormTypeSelector = "username" | "password";
