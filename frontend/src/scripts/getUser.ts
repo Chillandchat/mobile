5dc55728c9db56d4a5a539f9582e9b942a0f13d9
AlvinC888 <chengalvin333@gmail.com>
2022-02-02 14:22:29 +1100

    Create getUser.ts

---
+++ frontend/src/scripts/getUser.ts
@@ -0,0 +1,29 @@
+import { AxiosResponse } from "axios";
+import api from "./api";
+import { AuthType } from "../types/apiTypes";
+
+/**
+ * This function is responsible for returning a user's information from the remote server.
+ *
+ * @param {string} user The user to get information from.
+ * @returns {AuthType} This function returns the user's information.
+ * @see {AuthType} for more information about return type.
+ */
+
+const getUser = async (user: string): Promise<AuthType | any> => {
+  let userData: AuthType;
+  try {
+    api.instance
+      .get(`${api.endpoints.getUserInfo}?key=${api.apiKey}&user=${user}`)
+      .then((data: AxiosResponse): void => {
+        userData = data.data;
+      })
+      .catch((err: any): void => {
+        throw new Error(`API Error: ${err} \n   Error code: CC_ERROR_0318`);
+      });
+  } catch (err: any) {
+    throw new Error(`Error: ${err} \n   Error code: CC_ERROR_0022`);
+  }
+};
+
+export default getUser;
