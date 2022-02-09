570db0ae707b7a1770401cf7027d37ba7ce85148
AlvinC888 <chengalvin333@gmail.com>
2022-02-09 20:59:09 +1100

    WIP: Get all room endpoint

---
+++ Backend/api/endpoints/getAllRooms.ts
@@ -0,0 +1,9 @@
+// ! IN DEVELOPMENT:
+import { NextFunction, Request, Response } from "express";
+import roomSchema from "../schema/roomSchema";
+
+const getAllRooms = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {};
