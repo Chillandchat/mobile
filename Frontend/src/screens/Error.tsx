6c3723f6af778b006bc85ec32f56bed2aeb8d251
AlvinC888 <chengalvin333@gmail.com>
2022-02-12 17:39:25 +1100

    Update Error.tsx

--- Frontend/src/screens/Error.tsx
+++ Frontend/src/screens/Error.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, BackHandler } from "react-native";
+import { View, Text, StyleSheet, BackHandler, Linking } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import Button from "../components/Button";
 
@@ -15,17 +15,33 @@ const Error: React.FC = () => {
       fontSize: 30,
       padding: 18,
     },
+    text: {
+      fontFamily: "poppins",
+      fontSize: 17,
+      paddingBottom: 23,
+    },
   });
   return (
     <View style={style.container}>
       <MaterialIcons name="error" size={75} color="orange" />
-      <Text style={style.titleText}>Error!</Text>
+      <Text style={style.titleText}>Unknown error</Text>
+      <Text style={style.text}>Please restart chill&chat to continue.</Text>
       <Button
         color={"orange"}
         textColor={"#ffff"}
         text="quit application"
         onPress={() => {
           BackHandler.exitApp();
+        }}
+      />
+      <Button
+        color={"transparent"}
+        textColor={"#000"}
+        text="report bug"
+        onPress={() => {
+          Linking.openURL(
+            "https://github.com/Chill-and-chat/Chill-and-chat/issues/"
+          );
         }}
       />
     </View>
