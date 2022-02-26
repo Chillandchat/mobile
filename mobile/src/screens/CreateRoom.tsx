493979162c517d04ef03cdee251bedb891b3d62b
AlvinC888 <chengalvin333@gmail.com>
2022-02-26 14:14:01 +1100

    Update CreateRoom.tsx

--- mobile/src/screens/CreateRoom.tsx
+++ mobile/src/screens/CreateRoom.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Alert } from "react-native";
+import { useSelector } from "react-redux";
 import Button from "../components/Button";
 import Form from "../components/Form";
+import { RootState } from "../redux/index.d";
+import createRoom from "../scripts/createRoom";
 
-const CreateRoom: React.FC = () => {
+/**
+ * This is the create room page, this page will prompt the user to create a room.
+ */
+
+const CreateRoom: React.FC<any> = ({ navigation }) => {
   let name: string;
   let password: string;
+
+  const { username } = useSelector((state: RootState): RootState => {
+    return state.userInfo;
+  });
+
+  const [error, setError] = React.useState("");
 
   const style: any = StyleSheet.create({
     container: {
@@ -20,6 +33,12 @@ const CreateRoom: React.FC = () => {
       fontFamily: "poppinsExtraBold",
       fontSize: 25,
       padding: "15%",
+    },
+    error: {
+      color: "red",
+      marginTop: -20,
+      marginBottom: 20,
+      fontFamily: "poppinsLight",
     },
   });
 
@@ -46,11 +65,40 @@ const CreateRoom: React.FC = () => {
 
       <View style={style.divider} />
 
+      <Text style={style.error}>{error}</Text>
       <Button
         color={"#00AD98"}
         textColor={"#ffff"}
         text={"Lets' go!"}
-        onPress={(): void => {}}
+        onPress={(): void => {
+          if (name !== undefined && password !== undefined) {
+            createRoom(name, password, username)
+              .then((): void => {
+                Alert.alert("","Please restart the app to see the changes.");
+                navigation.navigate("menu");
+              })
+              .catch((err: unknown): void => {
+                setError("Unable to create room.");
+                console.error(err);
+                setTimeout((): void => {
+                  setError("");
+                }, 3000);
+              });
+          } else {
+            setError("Missing data.");
+            setTimeout((): void => {
+              setError("");
+            }, 3000);
+          }
+        }}
+      />
+      <Button
+        textColor="black"
+        color={"transparent"}
+        onPress={(): void => {
+          navigation.navigate("menu");
+        }}
+        text={"Cancel"}
       />
     </View>
   );
