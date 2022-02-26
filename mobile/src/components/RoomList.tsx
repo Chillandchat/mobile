034b50633cda328963c469785a40e67107a56dc8
AlvinC888 <chengalvin333@gmail.com>
2022-02-26 22:19:35 +1100

    Update RoomList.tsx

--- mobile/src/components/RoomList.tsx
+++ mobile/src/components/RoomList.tsx
@@ -6,6 +6,7 @@ import { RoomType } from "../scripts";
 import Icon from "./Icon";
 import { RoomListProps as Props } from "./index.d";
 import { v4 as uuid } from "uuid";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 /**
  * This is the room list component, this component will diaplay all the rooms provied.
@@ -29,27 +30,54 @@ const RoomList: React.FC<Props> = (props: Props) => {
       alignSelf: "flex-start",
       marginHorizontal: "5%",
       width: "100%",
+      marginBottom: "10%",
     },
+    error: {
+      fontFamily: "poppins",
+      fontSize: 20,
+      marginTop: 70,
+      opacity: 0.3,
+      marginBottom: 10,
+    },
+    errorContainer: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    sadFace: { opacity: 0.3 },
   });
 
-  return (
-    <ScrollView style={style.container}>
-      {props.rooms.map((room: RoomType): any => {
-        return (
-          <View style={style.roomContainer} key={uuid()}>
-            <Icon
-              iconLetter={room.name[0]}
-              color={room.iconColor}
-              key={uuid()}
-            />
-            <Text style={style.titleStyle} numberOfLines={1} key={uuid()}>
-              {room.name}
-            </Text>
-          </View>
-        );
-      })}
-    </ScrollView>
-  );
+  if (props.rooms.length === 0) {
+    return (
+      <View style={style.errorContainer}>
+        <Text style={style.error}>No rooms found.</Text>
+        <MaterialCommunityIcons
+          name="emoticon-sad-outline"
+          size={50}
+          color="black"
+          style={style.sadFace}
+        />
+      </View>
+    );
+  } else {
+    return (
+      <ScrollView style={style.container}>
+        {props.rooms.map((room: RoomType): any => {
+          return (
+            <View style={style.roomContainer} key={uuid()}>
+              <Icon
+                iconLetter={room.name[0]}
+                color={room.iconColor}
+                key={uuid()}
+              />
+              <Text style={style.titleStyle} numberOfLines={1} key={uuid()}>
+                {room.name}
+              </Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    );
+  }
 };
 
 export default RoomList;
