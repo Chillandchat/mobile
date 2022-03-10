d155e382127b3176edfa06b71c40a6d210986417
AlvinC888 <chengalvin333@gmail.com>
2022-03-10 18:42:01 +1100

    Merge socket and api servers

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,3 +1,4 @@
+import { MessageSchemaType } from "./index.d";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -17,9 +18,16 @@ import rateLimit from "express-rate-limit";
 import joinRoom from "./endpoints/joinRoom";
 import searchMessge from "./endpoints/SearchMessage";
 import debug from "./debug";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
+import message from "./schema/messageSchema";
 
 const app: express.Express = express();
-const PORT: string = process.env.PORT || "8080";
+const httpServer: any = createServer(app);
+const io = new Server(httpServer);
+
+const PORT: number = Number(process.env.PORT) || 3000;
+const SOCKET_PORT: number = Number(process.env.SOCKET_PORT) || 3001;
 
 const apiLimiter = rateLimit({
   windowMs: 1 * 30 * 1000,
@@ -50,6 +58,40 @@ app.post("/api/create-room", createRoom);
 app.post("/api/join-room", joinRoom);
 app.get("/site-map", siteMap);
 
+// Socket server:
+
+io.on("connection", (socket: Socket): void => {
+  socket.on(
+    "server-message",
+    async (
+      payload: MessageSchemaType,
+      key: string,
+      responseToken: string
+    ): Promise<void> => {
+      if (key === process.env.KEY) {
+        io.emit(`client-message:room(${payload.room})`, payload);
+        try {
+          const newMessage = new message({
+            id: payload.id,
+            user: payload.user,
+            content: payload.content,
+            room: payload.room,
+          });
+
+          await newMessage.save().then((): void => {
+            io.emit(`sent:token-${responseToken}`);
+            debug.log(`Message: ${payload.id} saved and emited.`);
+          });
+        } catch (err: unknown) {
+          io.emit(`error:token(${responseToken})`, err);
+        }
+      } else {
+        io.emit(`error:token(${responseToken})`, "Invalid key");
+      }
+    }
+  );
+});
+
 // DERECATED:
 app.post("/derecated/report-user", reportUser);
 
@@ -66,4 +108,8 @@ app.listen(PORT, (): void => {
   debug.log(
     `Server Ready and listening on port ${PORT}, press CTRL + C to stop operation.`
   );
+});
+
+httpServer.listen(SOCKET_PORT, (): void => {
+  debug.log(`Socket listening on port ${SOCKET_PORT}`);
 });
