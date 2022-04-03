69b8f0fc1cbc3a9e5a4353604a171b184754a7aa
AlvinC888 <chengalvin333@gmail.com>
2022-04-03 16:41:11 +1000

    Update getMessages.ts

--- src/scripts/getMessages.ts
+++ src/scripts/getMessages.ts
@@ -6,31 +6,21 @@ import api from "./api";
 /**
  * This function will return all messages in the database via the api.
  *
- * @optional @param room The room to get messages from.
+ * @param room The room to get messages from.
  * @returns {Promise<Array<MessageType>} The return type of the function, this function will return all messages.
  */
 
-const getMessages = async (room?: string): Promise<Array<MessageType>> => {
+const getMessages = async (room: string): Promise<Array<MessageType>> => {
   let messages: Array<MessageType> = [];
 
-    await api.instance
-      .get(`${api.endpoints.getMessages}?key=${api.apiKey}`)
-      .then((data: AxiosResponse): void => {
-        if (data.status !== 200) {
-          throw new Error(
-            `Request failed with status code: ${data.status} \n   Error code: CC_ERROR_0318`
-          );
-        } else {
-            data.data?.foreach((message: MessageType): void => {
-              if (message.room === room) {
-                messages.push(message);
-              }
-            });
-        }
-      })
-      .catch((err: unknown): void => {
-        throw new Error(`API Error: ${err} \n   Error code: CC_ERROR_0318`);
-      });
+  await api.instance
+    .get(`${api.endpoints.getMessages}?key=${api.apiKey}&room=${room}`)
+    .then((data: AxiosResponse): void => {
+      messages = data.data;
+    })
+    .catch((err: unknown): void => {
+      throw new Error(`API Error: ${err} \n   Error code: CC_ERROR_0318`);
+    });
 
   return messages;
 };
