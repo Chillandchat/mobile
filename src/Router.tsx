1468715590d90d5718cf50abca221f0b650a4cea
AlvinC888 <chengalvin333@gmail.com>
2022-04-10 15:08:25 +1000

    Update router

--- src/Router.tsx
+++ src/Router.tsx
@@ -35,135 +35,137 @@ import RoomInformation from "./screens/RoomInformation";
  * contains the stack for the navigation system and also loads important data in the application.
  */
 
-const Router: React.FC = () => {
-  const [loading, setLoading] = React.useState(true);
+namespace app {
+  export const Router: React.FC = () => {
+    const [loading, setLoading] = React.useState(true);
 
-  React.useEffect((): void => {
-    loadFonts()
-      .then((): void => {
-        setLoading(false);
-      })
-      .catch((err: string): void => {
-        Alert.alert(
-          "Error while loading fonts",
-          "An error occurred while trying to load fonts, try restarting the application or submit a issue report on Chill&chat offical github. \nError code: CC_ERROR_0015"
-        );
-        console.error(
-          "Error: Unable to load fonts. \n    at Router.tsx expoAppLoading.default \n    at loader.ts \n  Error code: CC_ERROR_0015"
-        );
-        console.error(`Expo error message: ${err}`);
+    React.useEffect((): void => {
+      loadFonts()
+        .then((): void => {
+          setLoading(false);
+        })
+        .catch((err: string): void => {
+          Alert.alert(
+            "Error while loading fonts",
+            "An error occurred while trying to load fonts, try restarting the application or submit a issue report on Chill&chat offical github. \nError code: CC_ERROR_0015"
+          );
+          console.error(
+            "Error: Unable to load fonts. \n    at Router.tsx expoAppLoading.default \n    at loader.ts \n  Error code: CC_ERROR_0015"
+          );
+          console.error(`Expo error message: ${err}`);
 
-        return;
-      });
-  }, []);
+          return;
+        });
+    }, []);
 
-  const NavigatorStack: TypedNavigator<
-    ParamListBase,
-    StackNavigationState<ParamListBase>,
-    StackNavigationOptions,
-    StackNavigationEventMap,
-    ({
-      initialRouteName,
-      children,
-      screenListeners,
-      screenOptions,
-      ...rest
-    }: any) => JSX.Element
-  > = createStackNavigator();
+    const NavigatorStack: TypedNavigator<
+      ParamListBase,
+      StackNavigationState<ParamListBase>,
+      StackNavigationOptions,
+      StackNavigationEventMap,
+      ({
+        initialRouteName,
+        children,
+        screenListeners,
+        screenOptions,
+        ...rest
+      }: any) => JSX.Element
+    > = createStackNavigator();
 
-  const style: any = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-  });
+    const style: any = StyleSheet.create({
+      container: {
+        flex: 1,
+      },
+    });
 
-  if (loading) {
-    return <AppLoading />;
-  } else {
-    return (
-      <Provider store={store}>
-        <View style={style.container}>
-          <StatusBar style="auto" />
-          <NavigationContainer>
-            <NavigatorStack.Navigator initialRouteName="login">
-              <NavigatorStack.Screen
-                name="login"
-                component={Login}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="information"
-                component={Information}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="sign-up"
-                component={Signup}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="menu"
-                component={Menu}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="room-details"
-                component={RoomInformation}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="chat"
-                component={Chat}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="signout-confirm"
-                component={SignoutConfirm}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="create-room"
-                component={CreateRoom}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="add-room"
-                component={AddRoom}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="join-room"
-                component={JoinRoom}
-                options={{ headerShown: false }}
-              />
-              <NavigatorStack.Screen
-                name="error"
-                component={Error}
-                options={{
-                  headerTitle: "Unexpected Error",
-                  headerStyle: {
-                    backgroundColor: "orange",
-                  },
-                  headerTintColor: "#fff",
-                }}
-              />
-              <NavigatorStack.Screen
-                name="block-error"
-                component={BlockError}
-                options={{
-                  headerTitle: "Unexpected Error",
-                  headerStyle: {
-                    backgroundColor: "orange",
-                  },
-                  headerTintColor: "#fff",
-                }}
-              />
-            </NavigatorStack.Navigator>
-          </NavigationContainer>
-        </View>
-      </Provider>
-    );
-  }
-};
+    if (loading) {
+      return <AppLoading />;
+    } else {
+      return (
+        <Provider store={store}>
+          <View style={style.container}>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+              <NavigatorStack.Navigator initialRouteName="login">
+                <NavigatorStack.Screen
+                  name="login"
+                  component={Login}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="information"
+                  component={Information}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="sign-up"
+                  component={Signup}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="menu"
+                  component={Menu}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="room-details"
+                  component={RoomInformation}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="chat"
+                  component={Chat}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="signout-confirm"
+                  component={SignoutConfirm}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="create-room"
+                  component={CreateRoom}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="add-room"
+                  component={AddRoom}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="join-room"
+                  component={JoinRoom}
+                  options={{ headerShown: false }}
+                />
+                <NavigatorStack.Screen
+                  name="error"
+                  component={Error}
+                  options={{
+                    headerTitle: "Unexpected Error",
+                    headerStyle: {
+                      backgroundColor: "orange",
+                    },
+                    headerTintColor: "#fff",
+                  }}
+                />
+                <NavigatorStack.Screen
+                  name="block-error"
+                  component={BlockError}
+                  options={{
+                    headerTitle: "Unexpected Error",
+                    headerStyle: {
+                      backgroundColor: "orange",
+                    },
+                    headerTintColor: "#fff",
+                  }}
+                />
+              </NavigatorStack.Navigator>
+            </NavigationContainer>
+          </View>
+        </Provider>
+      );
+    }
+  };
+}
 
-export default Router;
+export default app.Router;
