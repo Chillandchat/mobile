08bc3c8acfd71032ee45b22fea69bdcd75096a28
AlvinC888 <chengalvin333@gmail.com>
2022-04-26 17:00:26 +1000

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -5,16 +5,33 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 import { MessageProps } from "./index.d";
+import { AuthType } from "../scripts/index.d";
+import Icon from "./Icon";
 
 /**
  * This is the message component, this component will display the message that users send.
- * 
+ *
  * @prop {MessageType} message The message that the user sent.
  * @prop {string} user The id of the user currently logged in.
  * @prop {Array<AuthType>} roomUserInfo The information about all the users in the room
  */
 
 const Message: React.FC<MessageProps> = (props: MessageProps) => {
+  const [userInfo, setUserInfo]: any = React.useState({});
+
+  React.useEffect((): any => {
+    let foundUserInfo: boolean = false;
+
+    !foundUserInfo
+      ? props.roomUserInfo?.forEach((user: AuthType): void => {
+          if (user.username === props.message.user) {
+            setUserInfo(user);
+            foundUserInfo = true;
+          }
+        })
+      : null;
+  });
+
   const style: any = StyleSheet.create({
     container: {
       alignSelf: props.message.user === props.user ? "flex-end" : "flex-start",
@@ -33,15 +50,19 @@ const Message: React.FC<MessageProps> = (props: MessageProps) => {
     },
   });
 
+  // TODO: line 58 add positioning to icon component
   return (
     <View style={style.container} key={uuid()}>
-      {props.message.user !== props.user ? (
-        <Text
-          key={uuid()}
-          style={[style.content, { fontFamily: "poppinsBold", fontSize: 18 }]}
-        >
-          {props.message.user}
-        </Text>
+      {props.message.user !== userInfo.id ? (
+        <View>
+          <Icon iconLetter={userInfo.username[0]} color={userInfo.iconColor} />
+          <Text
+            key={uuid()}
+            style={[style.content, { fontFamily: "poppinsBold", fontSize: 18 }]}
+          >
+            {userInfo.username}
+          </Text>
+        </View>
       ) : null}
       <Text key={uuid()} style={style.content}>
         {props.message.content}
