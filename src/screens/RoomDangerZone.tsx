30b53f9f412b63439a875e4a4a648961db16450e
AlvinC888 <chengalvin333@gmail.com>
2022-07-03 19:27:10 +1000

    Update RoomDangerZone.tsx

--- src/screens/RoomDangerZone.tsx
+++ src/screens/RoomDangerZone.tsx
@@ -7,9 +7,12 @@ import { useSelector } from "react-redux";
 import reportRoom from "../scripts/reportRoom";
 import Button from "../components/Button";
 import { RootState } from "../redux/index.d";
+import removeRoom from "../scripts/removeRoom";
 
 const RoomDangerZone: React.FC = () => {
-  const { sessionStatus } = useSelector((state: RootState): RootState => state);
+  const { sessionStatus, userInfo }: any = useSelector(
+    (state: RootState): RootState => state
+  );
   const navigation: any = useNavigation();
 
   const style: any = StyleSheet.create({
@@ -53,7 +56,6 @@ const RoomDangerZone: React.FC = () => {
                 text: "Report",
                 style: "destructive",
                 onPress: (): void => {
-                  console.log("pressed");
                   reportRoom(sessionStatus.id)
                     .then((): void => {
                       Alert.alert(
@@ -71,6 +73,34 @@ const RoomDangerZone: React.FC = () => {
           );
         }}
         text={"report room"}
+      />
+      <Button
+        color={"red"}
+        textColor={"#fff"}
+        onPress={(): void => {
+          Alert.alert(
+            "",
+            "Are you sure you want to leave this room? You cannot join unless you are reinvited.",
+            [
+              {
+                text: "Leave",
+                style: "destructive",
+                onPress: (): void => {
+                  removeRoom(sessionStatus.id, userInfo.username)
+                    .then((): void => {
+                      navigation.navigate("login");
+                      Alert.alert("Room left", "You have left this room.");
+                    })
+                    .catch((err: unknown): void => {
+                      console.error(err);
+                    });
+                },
+              },
+              { text: "Cancel" },
+            ]
+          );
+        }}
+        text={"leave room"}
       />
     </View>
   );
