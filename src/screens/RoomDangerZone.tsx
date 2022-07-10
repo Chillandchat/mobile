8dc230effe37533395dd886c1e603b4cac9485c1
AlvinC888 <chengalvin333@gmail.com>
2022-07-10 15:59:57 +1000

    New information screen design

--- src/screens/RoomDangerZone.tsx
+++ src/screens/RoomDangerZone.tsx
@@ -18,7 +18,7 @@ const RoomDangerZone: React.FC = () => {
   const style: any = StyleSheet.create({
     text: {
       fontFamily: "poppinsBold",
-      fontSize: 17,
+      fontSize: 22,
       padding: 30,
     },
     back: {
@@ -30,6 +30,12 @@ const RoomDangerZone: React.FC = () => {
       justifyContent: "center",
       alignItems: "center",
       flex: 1,
+    },
+    buttons: {
+      borderColor: "red",
+      borderWidth: 2,
+      padding: 20
+      ,borderRadius: 10,
     },
   });
 
@@ -43,65 +49,68 @@ const RoomDangerZone: React.FC = () => {
       >
         <AntDesign name="back" size={24} color="black" />
       </TouchableOpacity>
-      <Text style={style.text}>Seriously, continue with caution! </Text>
-      <Button
-        color={"red"}
-        textColor={"#fff"}
-        onPress={(): void => {
-          Alert.alert(
-            "",
-            "Are you sure you want to report this room? The Chill&chat team will be notified once reported.",
-            [
-              {
-                text: "Report",
-                style: "destructive",
-                onPress: (): void => {
-                  reportRoom(sessionStatus.id)
-                    .then((): void => {
-                      Alert.alert(
-                        "Room reported",
-                        "This room has been reported, thank you for your feedback! We will take action as soon as possible."
-                      );
-                    })
-                    .catch((err: unknown): void => {
-                      console.error(err);
-                    });
+      <Text style={style.text}>Danger zone </Text>
+      <View style={style.buttons}>
+        <Button
+          color={"red"}
+          textColor={"#fff"}
+          onPress={(): void => {
+            Alert.alert(
+              "",
+              "Are you sure you want to report this room? The Chill&chat team will be notified once reported.",
+              [
+                {
+                  text: "Report",
+                  style: "destructive",
+                  onPress: (): void => {
+                    reportRoom(sessionStatus.id)
+                      .then((): void => {
+                        Alert.alert(
+                          "Room reported",
+                          "This room has been reported, thank you for your feedback! We will take action as soon as possible."
+                        );
+                      })
+                      .catch((err: unknown): void => {
+                        console.error(err);
+                      });
+                  },
                 },
-              },
-              { text: "Cancel" },
-            ]
-          );
-        }}
-        text={"report room"}
-      />
-      <Button
-        color={"red"}
-        textColor={"#fff"}
-        onPress={(): void => {
-          Alert.alert(
-            "",
-            "Are you sure you want to leave this room? You cannot join unless you are reinvited.",
-            [
-              {
-                text: "Leave",
-                style: "destructive",
-                onPress: (): void => {
-                  removeRoom(sessionStatus.id, userInfo.username)
-                    .then((): void => {
-                      navigation.navigate("login");
-                      Alert.alert("Room left", "You have left this room.");
-                    })
-                    .catch((err: unknown): void => {
-                      console.error(err);
-                    });
+                { text: "Cancel" },
+              ]
+            );
+          }}
+          text={"report room"}
+        />
+        <View style={{ marginTop: 10 }} />
+        <Button
+          color={"red"}
+          textColor={"#fff"}
+          onPress={(): void => {
+            Alert.alert(
+              "",
+              "Are you sure you want to leave this room? You cannot join unless you are reinvited.",
+              [
+                {
+                  text: "Leave",
+                  style: "destructive",
+                  onPress: (): void => {
+                    removeRoom(sessionStatus.id, userInfo.username)
+                      .then((): void => {
+                        navigation.navigate("login");
+                        Alert.alert("Room left", "You have left this room.");
+                      })
+                      .catch((err: unknown): void => {
+                        console.error(err);
+                      });
+                  },
                 },
-              },
-              { text: "Cancel" },
-            ]
-          );
-        }}
-        text={"leave room"}
-      />
+                { text: "Cancel" },
+              ]
+            );
+          }}
+          text={"leave room"}
+        />
+      </View>
     </View>
   );
 };
--- src/screens/RoomInformation.tsx
+++ src/screens/RoomInformation.tsx
@@ -1,21 +1,34 @@
 import React from "react";
-import { View, StyleSheet, Text, TouchableOpacity, Alert } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
 import { useSelector } from "react-redux";
 import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 import * as Clipboard from "expo-clipboard";
 
 import { RootState } from "../redux/index.d";
 import Button from "../components/Button";
+import { AuthType } from "../scripts";
+import Icon from "../components/Icon";
 
 /**
  * This is the room information screen, this screen will display the room information.
  */
 
 const RoomInformation: React.FC<any> = ({ navigation }) => {
-  const { sessionStatus } = useSelector((state: RootState): RootState => {
-    return state;
-  });
+  const { sessionStatus, roomUserInfo, userInfo } = useSelector(
+    (state: RootState): RootState => {
+      return state;
+    }
+  );
 
   const style: any = StyleSheet.create({
     container: {
@@ -41,6 +54,20 @@ const RoomInformation: React.FC<any> = ({ navigation }) => {
       position: "absolute",
       bottom: "7%",
     },
+    users: {
+      flexDirection: "row",
+      position: "absolute",
+      top: "15%",
+    },
+    userList: {
+      height: "60%",
+      width: "90%",
+    },
+    user: {
+      flexDirection: "row",
+      margin: 10,
+      alignItems: "center",
+    },
   });
 
   return (
@@ -53,26 +80,66 @@ const RoomInformation: React.FC<any> = ({ navigation }) => {
       >
         <AntDesign name="back" size={24} color="black" />
       </TouchableOpacity>
-      <Text style={[style.boldText, { fontSize: 20 }]}>Room information:</Text>
-      <View style={{ height: 10 }} />
-      <View style={{ flexDirection: "row" }}>
-        <AntDesign
-          name="idcard"
-          size={30}
-          color="black"
-          style={{ paddingHorizontal: 10 }}
-        />
-        <Text style={style.text}>{sessionStatus.id}</Text>
+      <View style={style.users}>
+        <Feather name="users" size={24} color="black" />
+        <Text style={[style.boldText, { fontSize: 25, marginLeft: 15 }]}>
+          Users:
+        </Text>
       </View>
-      <View style={{ flexDirection: "row" }}>
-        <Feather
-          name="users"
-          size={30}
-          color="black"
-          style={{ paddingHorizontal: 10 }}
-        />
-        <Text style={style.text}>{sessionStatus.users.length.toString()}</Text>
+
+      <View style={style.userList}>
+        <ScrollView>
+          <View style={style.user}>
+            <Icon
+              iconLetter={userInfo.username[0]}
+              color={userInfo.iconColor}
+            />
+            <Text
+              style={[
+                style.text,
+                { fontSize: 22, marginLeft: 20, marginRight: 10 },
+              ]}
+            >
+              {`${userInfo.username} (You)`}
+            </Text>
+            {userInfo.verified ? (
+              <MaterialIcons name="verified-user" size={24} color={"#00AD98"} />
+            ) : null}
+
+            {userInfo.bot ? (
+              <FontAwesome5 name="robot" size={24} color={"#00AD98"} />
+            ) : null}
+          </View>
+
+          {roomUserInfo.map((user: AuthType): any => {
+            return (
+              <View style={style.user}>
+                <Icon iconLetter={user.username[0]} color={user.iconColor} />
+                <Text
+                  style={[
+                    style.text,
+                    { fontSize: 22, marginLeft: 20, marginRight: 10 },
+                  ]}
+                >
+                  {user.username}
+                </Text>
+                {user.verified ? (
+                  <MaterialIcons
+                    name="verified-user"
+                    size={24}
+                    color={"#00AD98"}
+                  />
+                ) : null}
+
+                {user.bot ? (
+                  <FontAwesome5 name="robot" size={24} color={"#00AD98"} />
+                ) : null}
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
+
       <View style={style.dangerButton}>
         <TouchableOpacity
           onPress={(): void => {
@@ -93,15 +160,15 @@ const RoomInformation: React.FC<any> = ({ navigation }) => {
           />
           <Text style={style.text}>Copy information</Text>
         </TouchableOpacity>
-          <Button
-            color={"red"}
-            onPress={() => {
-              navigation.push("room-danger-zone");
-            }}
-            textColor={"#ffff"}
-            text={"Danger zone"}
-          />
-        </View>
+        <Button
+          color={"red"}
+          onPress={() => {
+            navigation.push("room-danger-zone");
+          }}
+          textColor={"#ffff"}
+          text={"Danger zone"}
+        />
+      </View>
     </View>
   );
 };
