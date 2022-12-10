6571921d40184b6fe38142f8d3b2e5934d62cde8
AlvinC888 <chengalvin333@gmail.com>
2022-12-10 13:00:45 +1100

    Create uploadContent.ts

---
+++ src/scripts/uploadContent.ts
@@ -0,0 +1,37 @@
+//! "import "react-native-get-random-values";" MUST BE FIRST!!
+import "react-native-get-random-values";
+import { AxiosResponse } from "axios";
+import { v4 as uuid } from "uuid";
+
+import { ContentType } from "./index.d";
+import api from "./api";
+
+/**
+ * This is the upload content script, this script will upload the content to the upload content endpoint.
+ *
+ * @param {string} user The user that the image is sent on behalf.
+ * @param {string} content The content of the image or gif.
+ * @param {ContentType} type The type of the content format in the content parameter.
+ *
+ * @note The content MUST be in a base-64 format!!!!
+ */
+
+const uploadContent = async (
+  user: string,
+  content: string,
+  type: ContentType
+): Promise<void> => {
+  await api.instance
+    .post(`${api.endpoints.uploadContent}?key=${api.apiKey}`, {
+      id: uuid(),
+      content: content,
+      type: type,
+      user: user,
+    })
+    .then((_data: AxiosResponse): void => {})
+    .catch((err: unknown): void => {
+      throw new Error(`API Error: ${err} \n   Error code: CC_ERROR_0318`);
+    });
+};
+
+export default uploadContent;
