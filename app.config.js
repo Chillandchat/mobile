fc4e944900807df568a4c38bc76b88784030a0aa
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-01-17 09:03:47 +1100

    Bump build number

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.chillandchat.mobile",
-      buildNumber: "8",
+      buildNumber: "9",
     },
     android: {
       adaptiveIcon: {
