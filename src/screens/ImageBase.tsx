485d1754a3c9824702c4a2d32a62f83c5d4b3a90
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-01-19 18:40:15 +1100

    Finish gif feature

--- src/screens/ImageBase.tsx
+++ src/screens/ImageBase.tsx
@@ -10,10 +10,13 @@ import {
   Platform,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { PrivateValueStore, useNavigation } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
 
 import Form from "../components/Form";
+import getGif from "../scripts/getGif";
+import { setImageBase } from "../redux/action";
+import Button from "../components/Button";
 
 /**
  * This is the image base component/screen.
@@ -22,8 +25,43 @@ import Form from "../components/Form";
 
 const ImageBase: React.FC = () => {
   const [filter, setFilter]: any = React.useState("");
+  const [results, setResults]: any = React.useState([]);
+  const [loading, setLoading]: any = React.useState(true);
+
   const navigation: any = useNavigation();
   const dispatch: any = useDispatch();
+
+  React.useEffect((): void => {
+    getGif(filter, filter === "")
+      .then((gif: Array<any>): void => {
+        setResults((_prev: any): any => {
+          return gif;
+        });
+      })
+      .catch((err: unknown): void => {
+        console.error(err);
+        navigation.navigate("error");
+      });
+  }, []);
+
+  React.useEffect((): void => {
+    filter === "" && results.length !== 0
+      ? getGif(filter, filter === "")
+          .then((gif: Array<any>): void => {
+            setResults((_prev: any): any => {
+              return gif;
+            });
+          })
+          .catch((err: unknown): void => {
+            console.error(err);
+            navigation.navigate("error");
+          })
+      : null;
+  }, [filter]);
+
+  React.useEffect((): void => {
+    if (results.length > 0) setLoading(false);
+  }, [results.length]);
 
   const style: any = StyleSheet.create({
     container: {
@@ -32,9 +70,18 @@ const ImageBase: React.FC = () => {
       alignItems: "center",
     },
     image: {
-      height: 300,
-      width: 300,
+      height: 150,
+      width: 150,
       borderRadius: 20,
+    },
+    searchWrapper: {
+      width: "100%",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    searchIcon: {
+      padding: 20,
     },
     back: {
       position: "absolute",
@@ -45,15 +92,21 @@ const ImageBase: React.FC = () => {
       backgroundColor: "#E5E5E5",
       margin: 20,
       borderRadius: 20,
-      height: 300,
-      width: 300,
+      height: 150,
+      width: 150,
     },
     imageListContainer: {
-      height: "55%",
+      height: "65%",
       width: "90%",
       justifyContent: "center",
-      alignItems: "center",
       marginTop: 30,
+      alignItems: "center",
+    },
+    _imageListContainerStyle: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      alignItems: "center",
     },
   });
 
@@ -72,24 +125,54 @@ const ImageBase: React.FC = () => {
         >
           <AntDesign name="back" size={24} color="black" />
         </TouchableOpacity>
-        <Form
-          placeholder="Search"
-          onTextChange={(text: string): void => {
-            setFilter(text);
-          }}
-        />
+        <View style={style.searchWrapper}>
+          <Form
+            placeholder="Search"
+            width={"70%"}
+            value={filter}
+            onTextChange={(text: string): void => {
+              setFilter(text);
+            }}
+          />
+          <TouchableOpacity
+            style={style.searchIcon}
+            onPress={(): void => {
+              getGif(filter, filter === "")
+                .then((gif: Array<any>): void => {
+                  setResults((_prev: any): any => {
+                    return gif;
+                  });
+                })
+                .catch((err: unknown): void => {
+                  console.error(err);
+                  navigation.navigate("error");
+                });
+            }}
+          >
+            <AntDesign name="search1" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
         <View style={style.imageListContainer}>
-          <ScrollView>
-            {/* <TouchableOpacity
-                  key={image.link}
-                  style={style.imageContainer}
-                  onPress={(): void => {
-                    dispatch(setImageBase(image.link));
-                    navigation.navigate("send-image");
-                  }}
-                >
-                  <Image source={{ uri: image.link }} style={style.image} />
-                </TouchableOpacity> */}
+          <ScrollView contentContainerStyle={style._imageListContainerStyle}>
+            {loading
+              ? null
+              : results.map((image: any): any => {
+                  return (
+                    <TouchableOpacity
+                      key={image.id}
+                      style={style.imageContainer}
+                      onPress={(): void => {
+                        dispatch(setImageBase(image.images.original.url));
+                        navigation.navigate("send-image");
+                      }}
+                    >
+                      <Image
+                        source={{ uri: image.images.original.url }}
+                        style={style.image}
+                      />
+                    </TouchableOpacity>
+                  );
+                })}
           </ScrollView>
         </View>
       </ScrollView>
