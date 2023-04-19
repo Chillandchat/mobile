04fdf006b5d9bffe1c804456998270dabb27f208
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-04-19 15:34:15 +1000

    Add ```AddRoomOptions``` screen

---
+++ src/screens/AddRoomOptions.tsx
@@ -0,0 +1,121 @@
+import { BarCodeScanner, PermissionResponse } from "expo-barcode-scanner";
+import React from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Dispatch } from "redux";
+
+import Button from "../components/Button";
+import { setScannerResult } from "../redux/action";
+
+/**
+ * This is the Add room options component located on the add room component.
+ * This component will prompt the user with more options around the rooms they can create.
+ */
+
+const AddRoomOptions: React.FC = () => {
+  const [scannerOn, setScannerOn]: any = React.useState(false);
+  const [hasPermission, setHasPermission]: any = React.useState(null);
+
+  const navigation: any = useNavigation();
+
+  const dispatch: Dispatch = useDispatch();
+
+  const style: any = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    scannerIcon: {
+      position: "absolute",
+      left: "7%",
+      bottom: "5%",
+    },
+    divider: {
+      padding: 20,
+    },
+    publicIcon: {
+      position: "absolute",
+      right: "7%",
+      bottom: "5%",
+    },
+    scanner: {
+      height: 300,
+      width: 300,
+      borderRadius: 20,
+      alignSelf: "center",
+    },
+    scannerBody: {
+      borderRadius: 20,
+      backgroundColor: "transparent",
+      height: 300,
+      width: 300,
+      zIndex: 2000,
+      overflow: "hidden",
+    },
+  });
+
+  return !scannerOn ? (
+    <View style={style.container}>
+      <Text>Options</Text>
+      <TouchableOpacity
+        style={style.scannerIcon}
+        onPress={async (): Promise<void> => {
+          await BarCodeScanner.requestPermissionsAsync().then(
+            (permission: PermissionResponse): void => {
+              setHasPermission(permission.status === "granted");
+            }
+          );
+
+          hasPermission
+            ? setScannerOn(true)
+            : await BarCodeScanner.requestPermissionsAsync().then(
+                (_permission: PermissionResponse): void => {
+                  setScannerOn(true);
+                }
+              );
+        }}
+      >
+        <AntDesign name="scan1" size={30} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={style.publicIcon}
+        onPress={(): void => {
+          navigation.goBack();
+          navigation.navigate("public-rooms");
+        }}
+      >
+        <MaterialIcons name="public" size={30} color="black" />
+      </TouchableOpacity>
+    </View>
+  ) : (
+    <View style={style.container}>
+      <View style={style.scannerBody}>
+        <BarCodeScanner
+          onBarCodeScanned={(data: any): void => {
+            if (!data.data.includes("!chillandchat-room-invite")) return;
+
+            dispatch(setScannerResult(data.data.slice(26, -1)));
+            navigation.goBack();
+            navigation.navigate("join-room");
+          }}
+          style={style.scanner}
+        />
+      </View>
+      <View style={style.divider} />
+      <Button
+        color={"transparent"}
+        textColor={"black"}
+        text={"Cancel"}
+        onPress={(): void => {
+          setScannerOn(false);
+        }}
+      />
+    </View>
+  );
+};
+
+export default AddRoomOptions;
